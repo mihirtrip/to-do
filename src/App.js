4cc4c379c0bebdbc291303259b323542e9bc8dcc
ankitadiaspark <ankita.tripathi@diaspark.com>
2024-10-27 02:05:33 +0530

    Updated task management functionality and fixed key prop issue

--- src/App.js
+++ src/App.js
@@ -3,87 +3,75 @@ import Taskform from './component/Taskform';
 import List_task from './component/List_task';
 import { useEffect, useState } from 'react';
 
-
 function App() {
-  const [tasks,setTasks] = useState([]);
-  useEffect(()=>{
-    
-    localStorage.setItem('tasks',JSON.stringify(tasks));
-    // console.log(tasks)
-    
-  },[tasks])
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
 
   useEffect(() => {
     const savedTasks = localStorage.getItem('tasks');
     if (savedTasks) {
-        setTasks(JSON.parse(savedTasks));  // Only parse if there are saved tasks
+      setTasks(JSON.parse(savedTasks));
     }
-}, []);
- 
+  }, []);
 
+  const addTask = (name) => {
+    setTasks((prev) => {
+      return [...prev, { id: Date.now(), name: name, done: false }]; // Generate unique ID
+    });
+  };
 
-  const addTask=(name)=>{
-    setTasks(prev=>{
-      return [...prev,{name:name,done:false}];
+  const updateTaskDone = (taskIndex, newDone) => {
+    setTasks((prev) => {
+      const newTask = [...prev];
+      newTask[taskIndex].done = newDone;
+      return newTask;
+    });
+  };
+
+  const numberComplete = tasks.filter((t) => t.done).length;
+  const total = tasks.length;
+
+  function getMessage() {
+    const percentage = (numberComplete / total) * 100;
+    if (percentage === 0) {
+      return 'Please start doing your chores';
+    } else if (total === 0) {
+      return 'No tasks';
+    } else if (percentage > 0 && percentage < 50) {
+      return 'More to go for halfway';
+    } else if (percentage === 50) {
+      return 'You are halfway there';
+    } else if (percentage > 50 && percentage < 100) {
+      return 'Just a few left';
+    } else {
+      return 'Hoola!!! You have completed all the tasks';
+    }
+  }
+
+  function removeTask(indexToRemove) {
+    setTasks((prev) => {
+      return prev.filter((_, index) => index !== indexToRemove);
     });
   }
 
-    const updateTaskDone =(taskIndex,newDone)=> {
-      setTasks(prev=>{
-        const newtask = [...prev];
-        newtask[taskIndex].done = newDone;
-        return newtask;
-      });
-    }
-    const numberComplete = tasks.filter(t =>t.done).length;
-    const total = tasks.length;
+  return (
+    <main>
+      <h1>{numberComplete}/{total} tasks complete</h1>
+      <h2>{getMessage()}</h2>
+      <Taskform onAdd={addTask} />
+      {tasks.map((task, index) => (
+        <List_task
+          {...task}
+          key={task.id} // Use the generated unique ID for the key
+          onTrash={() => removeTask(index)}
+          onToggle={done => updateTaskDone(index, done)}
+        />
+      ))}
+    </main>
+  );
+}
 
-
-    function getmessage(){
-      const percentage = numberComplete/total *100;
-      if(percentage===0){
-        return 'Please start doing your chors'
-
-      }
-      else if(total===0){
-        return 'not task';
-      }
-      else if(percentage >0 && percentage<50){
-        return 'more to go for halfway';
-      }
-      else if(percentage===50){
-        return 'you are halfway there';
-      }
-      else if(percentage>50 && percentage<100){
-        return 'just few left';
-      }
-      else{
-        return'hoola!!! you have completed all the task';
-      }
-      
-    }
-    function removeTask(indexToRemove){
-      setTasks(prev=>{
-        return prev.filter((taskobject,index)=>index!==indexToRemove);
-      })
-    }
-    
-    
-
-    return (
-      <main>
-        <h1>{numberComplete}/{total} task complete</h1>
-        <h2>{getmessage()}</h2>
-        <Taskform onAdd={addTask}/>
-        {tasks.map((task,index) => (
-          <List_task {...task} 
-          // onRename={newname=>renametask(index,newname)}
-          key = {task}
-          onTrash={()=>removeTask(index)}
-          onToggle={done => updateTaskDone(index,done)} />
-        ))}
-      </main> 
-    );
-  }
-
-  export default App;
+export default App;
